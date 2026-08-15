581c7f30ad5dde8278c316254ac916e986930f9d
Mayuri Puram <mayuripuram2008@gmail.com>
2026-08-15 12:45:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+ app.use(cors());
+ app.use(express.json());
+ app.use("/api/users", userRoutes);
+
+
+ mongoose.connect(process.env.MONGO_URL)
+ .then(() => {
+    console.log("MongoDB is connected");
+ })
+ .catch((error) => {
+    console.log("MongoDB is not connected error:" , error);
+
+ });
+
+ app.get("/", (req, res) => {
+    res.send("Event Managemnet & Ticket Booking Platform Server is ready");
+
+ });
+
+ const PORT = process.env.PORT || 5000;
+
+ app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+ });
